1aa3dfe41f1d5af2320db25e5a2276e5c38f6299
Nevermoi <wlgq@hotmail.com>
2019-11-26 09:07:30 +0800

    Update pyspark_jupyter_runner.sh

--- pyspark_jupyter_runner.sh
+++ pyspark_jupyter_runner.sh
@@ -1,4 +1,4 @@
-export PYSPARK_DRIVER_PYTHON="jupyter"
+export PYSPARK_DRIVER_PYTHON="jupyter" ## use set for windows
 export PYSPARK_DRIVER_PYTHON_OPTS="lab"
 export PYSPARK_PYTHON=python3
 pyspark --name App
